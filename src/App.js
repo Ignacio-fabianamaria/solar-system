00113d098db128a3c7a95582e1b43f8adadd8667
Siigmaf <faby.tbe@gmail.com>
2022-08-18 15:06:05 -0300

    criando arquivo Header e alteranco favicon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-// iniciando projeto
 import React from 'react';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <section>
+        <Header />
+      </section>
+    );
   }
 }
 
