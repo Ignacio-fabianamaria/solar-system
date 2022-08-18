df7cca4f4ec8d8081662f53252cfdc9efb70e821
Siigmaf <faby.tbe@gmail.com>
2022-08-18 18:00:24 -0300

    incluindo lista de missoes

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import MissionCard from './MissionCard';
 import Title from './Title';
-// import MissionCard from './MissionCard';
-// import missions from '../data/missions';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {// utilizando map para iterar o array de objetos missions e renderizar uma lista de missões
+          missions.map((missao) => (<MissionCard
+            key={ missao.name }// Atribuindo .name como identificador único
+            name={ missao.name }
+            year={ missao.year }
+            country={ missao.country }
+            destination={ missao.destination }
+          />))
+        }
       </div>
     );
   }
