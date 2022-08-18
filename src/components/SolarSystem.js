2d86438811775dcefb9be509311b1929354ab965
Siigmaf <faby.tbe@gmail.com>
2022-08-18 15:14:43 -0300

    criando componente SolarSystem

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system" className="sistema-sol">
+        <h3>Iniciando...</h3>
+      </div>
+    );
+  }
+}
+export default SolarSystem;
