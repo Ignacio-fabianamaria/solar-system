45cb05a4eb51c9046d1a8aaa1f2881ecfcf587fe
Siigmaf <faby.tbe@gmail.com>
2022-08-18 16:04:09 -0300

    criando componente PlanetCard

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">{ planetName }</p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+
+export default PlanetCard;
