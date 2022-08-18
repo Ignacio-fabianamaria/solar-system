710caa4b775be9bf8e12d9557e8eff508b81c1fa
Siigmaf <faby.tbe@gmail.com>
2022-08-18 17:47:23 -0300

    incluindo lista com os planetas

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system" className="sistema-sol">
         <Title headline="Planetas" />
+        {// utilizando map para iterar o array de objetos planets e renderizar uma lista com os planetas
+          planets.map((planeta) => (<PlanetCard // PlanetCard para renderizar cada elemento da lista
+            key={ planeta.name } // Atribuindo .name como identificador único
+            planetName={ planeta.name }
+            planetImage={ planeta.image }
+          />))
+        }
       </div>
     );
   }
