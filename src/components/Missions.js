1eec9c36bf656c99065c75c9fbc3e20318c3e99f
Siigmaf <faby.tbe@gmail.com>
2022-08-18 16:16:29 -0300

    Renderize  Title dentro do Missions.

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Title from './Title';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
-        <h1> iniciando...</h1>
+        <Title headline="Missões" />
       </div>
     );
   }
